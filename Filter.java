c72fbdcc5614cf5f3d8d0946875c75f0ee462381
Kevin Torres <79941554+KevinTorres74@users.noreply.github.com>
2025-03-13 01:13:04 -0600

    clases

---
+++ Filter.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Filter {
+
+    /**
+     * Inicializa este objeto de filtro a partir de las rutas del archivo de entrada y palabras comunes.
+     * @param inFilePath la ruta del archivo de entrada.
+     * @param commonFilePath la ruta del archivo con las palabras comunes.
+     * @throws IOException si alguno de los archivos no existe.
+     */
+    public Filter(String inFilePath, String commonFilePath) throws IOException {
+        // Aqui va su codigo
+    }
+
+
+    /**
+     * Crea un ArrayList de tokens
+     * @return un ArrayList de tokens
+     */
+    public ArrayList<String> createList() throws IOException {
+        //Aqui va su codigo
+    }
+}
